069d9bdae5ab5d9f435782faf4fd5ec6ad62fe87
William Getz <willegetz@gmail.com>
2025-09-18 09:13:41 -0700

    add the rest of the building sizes

--- src/templesAndShrines/templeConfiguration/buildingSize.js
+++ src/templesAndShrines/templeConfiguration/buildingSize.js
@@ -6,16 +6,24 @@ const buildingSize = function (rpgDiceRoller) {
 
     const buildingSizes = [
         { roll: 1, result: '1 story' },
+        { roll: 2, result: '2 stories' },
+        { roll: 3, result: '3 stories' },
         {
             roll: 4, result: 'multiple stories',
             action: function () {
                 const dieResult = d6.roll();
-
+                
                 const numberOfStories = dieResult + 3;
-
+                
                 return `${numberOfStories} stories`;
             }
-        }
+        },
+        { roll: 5, result: '1 underground level' },
+        { roll: 6, result: '1 underground Level + 1 story above ground' },
+        { roll: 7, result: '2 underground levels' },
+        { roll: 8, result: '2 underground levels + 1 story above ground' },
+        { roll: 9, result: '3 underground levels' },
+        { roll: 10, result: '3 underground levels + 1 story above ground' }
     ]
 
     const getBuildingSize = function () {
