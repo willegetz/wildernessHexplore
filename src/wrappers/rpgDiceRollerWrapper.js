c613427b7d4d62c4395616b58865f9d1def09b02
William Getz <willegetz@gmail.com>
2025-09-18 09:00:55 -0700

    introduce d6, refactor roll function for dice

--- src/wrappers/rpgDiceRollerWrapper.js
+++ src/wrappers/rpgDiceRollerWrapper.js
@@ -1,20 +1,30 @@
 'use strict';
 
-const rpgDiceRollerWrapper = (function(){
+const rpgDiceRollerWrapper = (function () {
     const rpgDiceRoller = require('@dice-roller/rpg-dice-roller');
 
+    const d6Die = new rpgDiceRoller.Dice.StandardDice(6, 1, {}, 1, 6, 'Standard 1 to 6 die');
     const d10Die = new rpgDiceRoller.Dice.StandardDice(10, 1, {}, 1, 10, 'Standard 1 to 10 die');
 
-    const d10 = function(){
+    const d6 = function () {
+        return rollFunction(d6Die);
+    }
+
+    const d10 = function () {
+        return rollFunction(d10Die);
+    }
+
+    const rollFunction = function (die) {
         return {
-            roll: function(){
-                const result = d10Die.roll().value;
+            roll: function () {
+                const result = die.roll().value;
                 return result;
             }
         }
     }
 
     return {
+        d6,
         d10
     }
 })();
