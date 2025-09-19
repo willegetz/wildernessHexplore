ac25206aa1c09d9a69f9555879bcf307f019d4c3
William Getz <willegetz@gmail.com>
2025-09-19 07:18:46 -0700

    add d8

--- src/wrappers/rpgDiceRollerWrapper.js
+++ src/wrappers/rpgDiceRollerWrapper.js
@@ -5,6 +5,7 @@ const rpgDiceRollerWrapper = (function () {
 
     const d4Die = new rpgDiceRoller.Dice.StandardDice(4, 1, {}, 1, 4, 'Standard 1 to 4 die');
     const d6Die = new rpgDiceRoller.Dice.StandardDice(6, 1, {}, 1, 6, 'Standard 1 to 6 die');
+    const d8Die = new rpgDiceRoller.Dice.StandardDice(8, 1, {}, 1, 8, 'Standard 1 to 8 die');
     const d10Die = new rpgDiceRoller.Dice.StandardDice(10, 1, {}, 1, 10, 'Standard 1 to 10 die');
     const d20Die = new rpgDiceRoller.Dice.StandardDice(20, 1, {}, 1, 20, 'Standard 1 to 20 die');
     const d100Die = new rpgDiceRoller.Dice.StandardDice(1000, 1, {}, 1, 100, 'Standard 1 to 100 die');
@@ -15,6 +16,10 @@ const rpgDiceRollerWrapper = (function () {
 
     const d6 = function () {
         return rollFunction(d6Die);
+    }
+
+    const d8 = function () {
+        return rollFunction(d8Die);
     }
 
     const d10 = function () {
@@ -41,6 +46,7 @@ const rpgDiceRollerWrapper = (function () {
     return {
         d4,
         d6,
+        d8,
         d10,
         d20,
         d100
