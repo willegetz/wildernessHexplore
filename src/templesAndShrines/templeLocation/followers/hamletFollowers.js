b249f7f741cd0ffe85a7f004f8505a4f2a9e68f6
William Getz <willegetz@gmail.com>
2025-09-18 21:13:30 -0700

    hamletFollowers getFollowerCount returns "2" when a 1 is rolled folled by a 1

---
+++ src/templesAndShrines/templeLocation/followers/hamletFollowers.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const hamletFollowers = function (rpgDiceRoller) {
+    const d4 = rpgDiceRoller.d4();
+    const d6 = rpgDiceRoller.d6();
+
+    const hamletFollowers = [
+        {
+            roll: 1, result: '1d4 +1 followers', action: function () {
+                const dieResult = d4.roll();
+
+                const followers = dieResult + 1;
+
+                return followers;
+            }
+        }
+    ]
+
+    const getFollowerCount = function () {
+        const dieResult = d6.roll();
+
+        const followerCount = hamletFollowers.filter(x => x.roll === dieResult)[0];
+
+        const totalFollowerCount = followerCount.action();
+
+        return totalFollowerCount;
+    }
+
+    return {
+        getFollowerCount
+    }
+};
+
+module.exports = hamletFollowers;
---
+++ tests/templesAndShrines/templeLocation/followers/hamletFollowers.tests.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const assert = require('assert');
+const sinon = require('sinon');
+
+const hamletFollowersModule = require('../../../../src/templesAndShrines/templeLocation/followers/hamletFollowers');
+const rpgDiceRollerWrapper = require('../../../../src/wrappers/rpgDiceRollerWrapper');
+
+describe('hamletFollowers', function () {
+    let d6Stub;
+    let d4Stub;
+
+    this.afterEach(() => {
+        if (d4Stub !== undefined) {
+            d4Stub.restore();
+        }
+
+        if (d6Stub !== undefined) {
+            d6Stub.restore();
+        }
+    });
+
+    describe('getFollowerCount', function () {
+        it('returns "2" when a 1 is rolled folled by a 1', function () {
+            d4Stub = sinon.stub(rpgDiceRollerWrapper, 'd4').returns({ roll: () => 1 });
+            d6Stub = sinon.stub(rpgDiceRollerWrapper, 'd6').returns({ roll: () => 1 });
+
+            const hamletFollowers = hamletFollowersModule(rpgDiceRollerWrapper);
+
+            const totalFollowers = hamletFollowers.getFollowerCount();
+
+            assert.equal(totalFollowers, 2);
+        });
+    });
+});
