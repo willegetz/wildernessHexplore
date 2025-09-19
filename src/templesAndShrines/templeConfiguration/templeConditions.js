38a1325a2e2962c4ac1cc219a769ccbde17d147b
William Getz <willegetz@gmail.com>
2025-09-18 20:37:12 -0700

    add the rest of the temple conditions

--- src/templesAndShrines/templeConfiguration/templeConditions.js
+++ src/templesAndShrines/templeConfiguration/templeConditions.js
@@ -6,6 +6,13 @@ const templeConditions = function (rpgDiceRoller) {
     const templeConditions = [
         {roll: 1, result: 'under construction'},
         {roll: 2, result: 'brand new'},
+        {roll: 3, result: 'slightly aged'},
+        {roll: 4, result: 'worn'},
+        {roll: 5, result: 'aged'},
+        {roll: 6, result: 'ancient'},
+        {roll: 7, result: 'crumbling'},
+        {roll: 8, result: 'tumbled down'},
+        {roll: 9, result: 'ruins'},
         {roll: 10, result: 'foundation only'}
     ]
 
