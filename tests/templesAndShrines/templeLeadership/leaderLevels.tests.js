39a3d2daa0bcec27d84cc446def72314a1897ebf
William Getz <willegetz@gmail.com>
2025-09-22 06:29:52 -0700

    leaderLevels getLeaderLevel 11th +1d8 returns "19th level" when a 19 is rolled followed by a 8

--- tests/templesAndShrines/templeLeadership/leaderLevels.tests.js
+++ tests/templesAndShrines/templeLeadership/leaderLevels.tests.js
@@ -24,13 +24,24 @@ describe('leaderLevels', function () {
         describe('11th +1d8', function () {
             it('returns "12th level" when a 19 is rolled followed by a 1', function () {
                 d8Stub = sinon.stub(rpgDiceRollerWrapper, 'd8').returns({ roll: () => 1 });
-                d20Stub = sinon.stub(rpgDiceRollerWrapper, 'd20').returns({roll: () => 19});
+                d20Stub = sinon.stub(rpgDiceRollerWrapper, 'd20').returns({ roll: () => 19 });
 
                 let leaderLevels = leaderLevelsModule(rpgDiceRollerWrapper);
 
                 const leaderLevel = leaderLevels.getLeaderLevel();
 
                 assert.equal('12th level', leaderLevel);
+            });
+
+            it('returns "19th level" when a 19 is rolled followed by a 8', function () {
+                d8Stub = sinon.stub(rpgDiceRollerWrapper, 'd8').returns({ roll: () => 8 });
+                d20Stub = sinon.stub(rpgDiceRollerWrapper, 'd20').returns({ roll: () => 19 });
+
+                let leaderLevels = leaderLevelsModule(rpgDiceRollerWrapper);
+
+                const leaderLevel = leaderLevels.getLeaderLevel();
+
+                assert.equal('19th level', leaderLevel);
             });
         });
     });
